0974ad61b43d8618954fba6c1d84964688648b91
kyashvardhan <68836374+kyashvardhan@users.noreply.github.com>
2025-02-04 21:38:57 +0530

    Add files via upload
    lfg with php...

---
+++ Practice/BasicCalculator.php
@@ -0,0 +1,22 @@
+<?php
+//First Program in Php.
+// two values initialize
+$valone = 4;
+$valtwo = 6;
+
+//addition
+$func_add = $a+$b;
+echo "The Addition of $valone and $valtwo is $func_add \n";
+
+//sub
+$func_sub = $valone-$valtwo;
+echo "The Sub of $valone and $valtwo is $func_sub \n";
+
+//mul
+$func_mul = $valone*$valtwo;
+echo "The Mul of $valone and $valtwo is $func_mul \n";
+
+//div
+$func_div = $valone/$valtwo;
+echo "The Div of $valone and $valtwo is $func_div \n";
+?>
