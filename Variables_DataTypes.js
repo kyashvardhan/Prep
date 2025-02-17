fb3e9b83d221bfd0ce6eacc42396e3640059e016
Yashvardhan Kapse <68836374+kyashvardhan@users.noreply.github.com>
2025-02-17 23:55:23 +0530

    Variables & Data Types
    Problem 1: Variables & Data Types
    💡 Task: Declare three variables and store:

---
+++ Variables_DataTypes.js
@@ -0,0 +1,19 @@
+/*Problem 1: Variables & Data Types
+💡 Task: Declare three variables and store:
+
+Your name (string)
+Your age (number)
+Are you a JavaScript learner? (boolean) */
+
+var name='yash';
+var age=21;
+var que="Are you a JavaScript learner?";
+Boolean(que);
+
+//console.log("My name is"+ $name + "and" + $age + "and if you ask me about js" + $que);
+
+console.log(name);
+console.log(age);
+console.log(que);
+
+
