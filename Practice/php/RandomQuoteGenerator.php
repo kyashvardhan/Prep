4b816f8b5d45028145e9ac95b07ecf064331fa51
Yashvardhan Kapse <68836374+kyashvardhan@users.noreply.github.com>
2025-02-28 22:45:17 +0530

    Create  RandomQuoteGenerator.php
    Added quotes.php with motivational quotes
    Optimized array selection for better efficiency

---
+++ Practice/php/RandomQuoteGenerator.php
@@ -0,0 +1,10 @@
+<?php
+$quotes = [
+    "Believe in yourself!",
+    "Keep pushing forward!",
+    "Never give up!",
+    "Success is the best revenge!",
+    "Dream big, work hard!"
+];
+echo $quotes[array_rand($quotes)];
+?>
