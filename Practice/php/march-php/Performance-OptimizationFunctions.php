a995a9809a5cd88b0bcec4504fadad8c86fd742d
Yashvardhan Kapse <68836374+kyashvardhan@users.noreply.github.com>
2025-03-02 21:29:25 +0530

    Update Performance-OptimizationFunctions.php
    fix: Improve caching function to auto-refresh expired data

--- Practice/php/march-php/Performance-OptimizationFunctions.php
+++ Practice/php/march-php/Performance-OptimizationFunctions.php
@@ -5,3 +5,12 @@ function cache_data($key, $data, $expiration = 3600) {
 function get_cached_data($key) {
     return get_transient($key);
 }
+
+function fetch_or_cache_data($key, $callback, $expiration = 3600) {
+    $data = get_transient($key);
+    if (!$data) {
+        $data = $callback();
+        set_transient($key, $data, $expiration);
+    }
+    return $data;
+}
