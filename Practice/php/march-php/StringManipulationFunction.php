6a7458a57938adf6d4a3eb3c101262be8bd8b4c3
Yashvardhan Kapse <68836374+kyashvardhan@users.noreply.github.com>
2025-03-02 21:18:32 +0530

    Update StringManipulationFunction.php
    Add custom function to reverse a string manually

--- Practice/php/march-php/StringManipulationFunction.php
+++ Practice/php/march-php/StringManipulationFunction.php
@@ -1,3 +1,11 @@
 function convert_to_title_case($string) {
     return ucwords(strtolower($string));
 }
+
+function reverse_string($string) {
+    $reversed = "";
+    for ($i = strlen($string) - 1; $i >= 0; $i--) {
+        $reversed .= $string[$i];
+    }
+    return $reversed;
+}
