dfc382dc0ee7bc64cb672d4a3f6c6780dfcf1ba8
Yashvardhan Kapse <68836374+kyashvardhan@users.noreply.github.com>
2025-03-03 20:39:58 +0530

    Create associative_array.php
    Added PHP associative array example with loops

---
+++ Practice/php/march-php/associative_array.php
@@ -0,0 +1,13 @@
+<?php
+// Associative array of users
+$users = [
+    "Yash" => 25,
+    "Virat" => 30,
+    "Rohit" => 22
+];
+
+// Loop through the array
+foreach ($users as $name => $age) {
+    echo "$name is $age years old.\n";
+}
+?>
