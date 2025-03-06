b34278126c080ab6cdde72d0a82ed3a7a8125d55
Yashvardhan Kapse <68836374+kyashvardhan@users.noreply.github.com>
2025-03-06 23:50:59 +0530

    Create modal_popup.js
    Developed Modal Popup Logic

---
+++ Practice/js/modal_popup.js
@@ -0,0 +1,3 @@
+const modal = document.getElementById("myModal");
+document.getElementById("openModal").onclick = () => modal.style.display = "block";
+document.getElementById("closeModal").onclick = () => modal.style.display = "none";
