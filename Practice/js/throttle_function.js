0375a582754dfb9dcd86bba27e26a5a4070b779f
Yashvardhan Kapse <68836374+kyashvardhan@users.noreply.github.com>
2025-03-06 23:48:49 +0530

    Create throttle_function.js
    Created Throttle Function

---
+++ Practice/js/throttle_function.js
@@ -0,0 +1,15 @@
+function throttle(func, limit) {
+  let inThrottle;
+  return function (...args) {
+    if (!inThrottle) {
+      func.apply(this, args);
+      inThrottle = true;
+      setTimeout(() => (inThrottle = false), limit);
+    }
+  };
+}
+
+window.addEventListener(
+  "scroll",
+  throttle(() => console.log("Scrolling..."), 200)
+);
