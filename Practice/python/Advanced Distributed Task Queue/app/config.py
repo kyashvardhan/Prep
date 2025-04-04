8f3b6badf6eaa1294797ec3f6f425c9c8ef6f4d6
Yashvardhan Kapse <68836374+kyashvardhan@users.noreply.github.com>
2025-04-04 23:37:00 +0530

    Create config.py
    added configuration for Celery and Redis integration

---
+++ Practice/python/Advanced Distributed Task Queue/app/config.py
@@ -0,0 +1,8 @@
+# app/config.py
+
+import os
+
+# Celery & Redis configuration
+REDIS_URL = os.getenv("REDIS_URL", "redis://localhost:6379/0")
+CELERY_BROKER_URL = REDIS_URL
+CELERY_RESULT_BACKEND = REDIS_URL
