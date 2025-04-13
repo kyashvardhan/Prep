bbe995d9de33ee0a07daa32f1245907264c293f5
Yashvardhan Kapse <68836374+kyashvardhan@users.noreply.github.com>
2025-04-13 23:42:38 +0530

    Create vowel_counter.c
    add program to count vowels in a given string

---
+++ Practice/C/vowel_counter.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <ctype.h>
+
+int countVowels(const char *str) {
+    int count = 0;
+    while (*str) {
+        char ch = tolower(*str);
+        if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u')
+            count++;
+        str++;
+    }
+    return count;
+}
+
+int main() {
+    char str[200];
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+    
+    int vowels = countVowels(str);
+    printf("Number of vowels: %d\n", vowels);
+    return 0;
+}
